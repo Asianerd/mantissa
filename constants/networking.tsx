49fae4d0c9c60255a43929376004ba8a1820ec5a
ajian_nedo <asianerd28@gmail.com>
2024-06-29 19:45:27 +0800

    Backend configuration

---
+++ constants/networking.tsx
@@ -0,0 +1,9 @@
+// export const SOTERIUS_BACKEND = 'http://127.0.0.1:8100';
+// export const SOTERIUS_BACKEND = 'http://192.168.68.103:8100';
+export const MANTISSA_BACKEND = 'http://127.0.0.1:8003';
+
+export const SOTERIUS_BACKEND = 'https://ozmium.xyz/soterius_backend';
+// export const MANTISSA_BACKEND = 'https://ozmium.xyz/mantissa_backend';
+
+// http networking is a pain in the ass for no reason
+// resorting to just prod directly
